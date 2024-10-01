f1e3cb86b3ab24d79f5b6bc07db42c354adc94a9
adminera <burntcharcoal@proton.me>
2024-10-01 19:03:10 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,6 @@
+// shows welcome message
+public class welcomemsg {
+  public static void main(String[] args) {
+    System.out.println("Welcome to passave, the only app you need to safely store your passwords!");
+  }
+}
