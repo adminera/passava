e4d3bff0ac4610da681faa4b95aec5475101d447
adminera <burntcharcoal@proton.me>
2024-10-14 18:24:49 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,17 @@
+// imports
+
+import java.util.Scanner; 
+
+
 // shows welcome message
 public class Main {
   public static void main(String[] args) {
     System.out.println("Welcome to passave, the only app you need to safely store your passwords!");
+
+
+    Scanner 
+
+
   }
 }
+
